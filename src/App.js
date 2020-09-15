bbe1d4687ec5090ec7abc3fe55ba1e47db1fa10c
Shu <stbook.syu@icloud.com>
2020-09-15 13:23:35 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+import Products from "./com";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+      filterdProducts: []
+    };
+  }
+  componentWillMount() {
+    fetch("http://locathost:8000/products")
+      .then(res => res.json())
+      .then(data =>
+        this.setState({
+          products: data,
+          filerdProducts: data
+        })
+      );
+  }
+  render() {
+    return (
+      <div className="container">
+        <h1>Ecommerce Shopping Cart Application</h1>
+        <hr />
+        <div className="row">
+          <div className="col-md-8">
+            <Products
+              products={this.state.filterdProducts}
+              handleAddToCart={this.handleAddToCart}
+            />
+          </div>
+          <div className="col-md-4"></div>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
